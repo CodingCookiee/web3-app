121d274e737285c92f165c503e6e32f634235986
Raza Awan <rzqutubawan@gmail.com>
2025-03-20 17:57:43 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App font-semibold text-2xl text-center">
+      <h1>React App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ui/WalletInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WalletInfo = () => {
+    return (
+        <div>
+            <h1>Wallet Info</h1>
+        </div>
+    );
+}
+
+export default WalletInfo;
